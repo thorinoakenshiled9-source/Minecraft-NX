87e48a64d43460f5de5cbae7d96ef247bee9c5e9
thorinoakenshiled9-source <thorinoakenshiled9@gmail.com>
2026-09-20 23:27:08 -0300

    Update main.c

--- source/main.c
+++ source/main.c
@@ -5,21 +5,24 @@ int main(int argc, char **argv)
 {
     consoleInit(NULL);
 
-    printf("\x1b[2J");
-    printf("Minecraft-NX - Diagnostic v0.2\n");
-    printf("==============================\n\n");
+    printf("Minecraft-NX - libnx test\n\n");
+    printf("Hello from Nintendo Switch!\n");
+    printf("Se esta tela apareceu, o NRO funciona.\n\n");
+    printf("Pressione + para sair.\n");
 
-    printf("[OK] NRO iniciou\n");
-    printf("[OK] libnx iniciou\n");
-    printf("[OK] consoleInit iniciou\n\n");
-
-    printf("Teste minimo: sem HID, JVM, LWJGL ou Minecraft.\n");
-    printf("Se voce ve esta tela, o bootstrap basico funciona.\n\n");
-
-    printf("Use HOME para voltar ao menu quando terminar.\n");
+    PadState pad;
+    padConfigureInput(1, HidNpadStyleSet_NpadStandard);
+    padInitializeDefault(&pad);
 
     while (appletMainLoop())
     {
+        padUpdate(&pad);
+
+        u64 kDown = padGetButtonsDown(&pad);
+
+        if (kDown & HidNpadButton_Plus)
+            break;
+
         consoleUpdate(NULL);
     }
 
